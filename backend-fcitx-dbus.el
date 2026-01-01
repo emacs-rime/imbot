e4c3964c62d08c992dba026dce27882fc3a680ba
Q <qiang.fang@zoho.com.cn>
2026-01-01 10:50:19 +0800

    switched to fcitx

---
+++ backend-fcitx-dbus.el
@@ -0,0 +1,218 @@
+;;; fcitx-dbus-backend.el *- lexical-binding: t; -*-
+
+(require 'dbus)
+
+;; https://github.com/fcitx/fcitx5/discussions/350
+;; dbus 接口的定义：
+;; https://github.com/fcitx/fcitx5-qt/blob/master/qt5/dbusaddons/interfaces/org.fcitx.Fcitx.InputContext1.xml
+;; https://github.com/fcitx/fcitx5-qt/blob/master/qt5/dbusaddons/interfaces/org.fcitx.Fcitx.InputMethod1.xml
+
+;; 输入法状态是和每个 input context 绑定的
+;; 首先 CreateInputContext，可以理解为创建了一个会话/连接，然后返回一个 dbus object path
+;; 之后继续用这个 object 和 fcitx 互相通信，发送按键，通过dbus signal获取预编辑和候选词列表
+;; 可以使用以下命令查看发送的事件：
+;; dbus-monitor --session "type='signal',sender='org.fcitx.Fcitx5'"
+;; dbus-monitor "interface='org.fcitx.Fcitx.InputContext1'"
+
+(defvar fcitx-service "org.fcitx.Fcitx5")
+(defvar fcitx-ic-tooltip nil)
+(defvar fcitx-ic-path nil)
+(defvar fcitx-ic-interface "org.fcitx.Fcitx.InputContext1")
+(defvar imbot-backend-im "rime")
+(defvar fcitx-ic-commit-string nil)
+
+(defun fcitx-alive ()
+  "Check if theres a running fcitx."
+  (dbus-ping :session fcitx-service 100))
+
+(defun fcitx-find-correct-service ()
+  "List all registered D-Bus services containing 'Fcitx'."
+  (interactive)
+  (let ((services (dbus-call-method :session "org.freedesktop.DBus"
+                                    "/org/freedesktop/DBus"
+                                    "org.freedesktop.DBus"
+                                    "ListNames")))
+    (message "Found Fcitx-related services: %s"
+             (seq-filter (lambda (s) (string-match-p "Fcitx" s)) services))))
+
+(defun fcitx-list-all-im ()
+  "Get a list of all available input methods and their unique names."
+  (interactive)
+  (let ((im-list (dbus-call-method :session "org.fcitx.Fcitx5"
+                                   "/controller"
+                                   "org.fcitx.Fcitx.Controller1"
+                                   "AvailableInputMethods")))
+    (with-current-buffer (get-buffer-create "*fcitx-engines*")
+      (erase-buffer)
+      (dolist (im im-list)
+        ;; im is a list: (name native-name icon-name unique-name)
+        (insert (format "Name: %s | ID: %s\n" (car im) (nth 3 im))))
+      (display-buffer (current-buffer)))
+    (message "Listed %d engines in *fcitx-engines*" (length im-list))))
+
+(defun fcitx-get-current-im ()
+  "Get the unique name of the currently active Input Method."
+  (interactive)
+  (let ((im (dbus-call-method :session "org.fcitx.Fcitx5"
+                              "/controller"
+                              "org.fcitx.Fcitx.Controller1"
+                              "CurrentInputMethod")))
+    (message "Current IM: %s" im)
+    im))
+
+(defun fcitx-ic-call-method (method &rest args)
+  (apply 'dbus-call-method `(:session ,fcitx-service ,fcitx-ic-path ,fcitx-ic-interface
+                                      ,method ,@args)))
+
+(defun fcitx-controller-call-method (method &rest args)
+  (apply 'dbus-call-method `(:session ,fcitx-service "/controller" "org.fcitx.Fcitx.Controller1"
+                                      ,method ,@args)))
+
+;; If the CreateInputContext method requires input arguments (as D-Bus methods often do),
+;; you would append them as additional arguments to the function call.
+;; You can use dbus-introspect-get-signature to determine the exact arguments required for the method.
+(defun fcitx-create-input-context (client-name)
+  "Input argument: A single string (DBus type s) named client_name.
+   Return type: A single object path (DBus type o).
+
+The object path returned points to the newly created input context object,
+ which implements the org.fcitx.Fcitx.InputContext1 interface (or similar).
+You then interact with this new object path for input method operations. "
+  (let ((ic (dbus-call-method :session fcitx-service
+                              "/org/freedesktop/portal/inputmethod"
+                              "org.fcitx.Fcitx.InputMethod1"
+                              "CreateInputContext"
+                              `((:struct "program" ,client-name)
+                                (:struct "display" "emacs")))))
+    (setq fcitx-ic-path (car ic))
+    ;; set capability CapabilityFlag::ClientSideInputPanel = (1ULL << 39)
+    (fcitx-ic-call-method "SetCapability" :uint64
+                          (logior
+                           (ash 1 39)
+                           ;; ClientSideControlState
+                           (ash 1 2)))
+    (dbus-register-signal :session fcitx-service
+                          fcitx-ic-path fcitx-ic-interface "CommitString"
+                          'fcitx-handler-for-commit-string)
+    (dbus-register-signal :session fcitx-service
+                          fcitx-ic-path fcitx-ic-interface "UpdateClientSideUI"
+                          'fcitx-handler-for-client-ui)))
+
+;; (s str)
+(defun fcitx-handler-for-commit-string (s)
+  (setq fcitx-ic-commit-string s))
+
+(defun fcitx-handler-for-client-ui (&rest tooltip)
+  (setq fcitx-ic-tooltip tooltip))
+
+;; backend interface functions
+(defun imbot-backend-activate ()
+  (unless fcitx-ic-path
+    (fcitx-create-input-context (number-to-string (round (time-to-seconds)))))
+  (fcitx-ic-call-method "FocusIn")
+  ;; im is a string, such as pinyin, rime
+  (fcitx-controller-call-method "SetCurrentIM" :string imbot-backend-im))
+
+;; keycode can be looked up in keyboard.py
+;; keyval can be looked up in keysyms.py
+;; or use xev for keysym and keycode
+;; ProcessKeyEvent(u keyval, u keycode, u state, b type, u time) = (b ret)
+;; bool processKeyEvent
+;; (uint32_t keyval, uint32_t keycode, uint32_t state, bool isRelease, uint32_t time)
+;; state representing the state of modifier keys (like Shift, Ctrl, Alt) at the time of the event. nil suggests no modifiers were active or the state is not specified. (shift: state 1
+;; The last argument, which likely provides a timestamp for the event, probably in milliseconds since a certain epoch, for timing purposes.
+;; ProcessKeyEvent(code, 0, mask, false, 0)
+;; nil (False) for the type parameter usually means Key Release in some DBus specs, or Key Press depending on the specific implementation. For Fcitx, usually 0 is press and 1 is release. Ensure you are sending a "Press" event to trigger a response.
+
+;; key event states
+;; Modifier	X11 Bitmask Value
+;; Shift	(ash 1 0) → 1
+;; Lock	(ash 1 1) → 2
+;; Control	(ash 1 2) → 4
+;; Alt/Meta	(ash 1 3) → 8
+
+(defun fcitx-process-key (keysym state)
+  (fcitx-ic-call-method "ProcessKeyEvent" keysym 0 state nil 0))
+
+;; backend specific key definition
+(defvar imbot-backend-menu-keys `(("M-n" . #xFF56) ; Next PageDown
+                                  ("M-p" . #xFF55) ; Prior PageUp
+                                  ("C-n" . #xFF54) ; C-n Down
+                                  ("C-p" . #xFF52) ; Up
+                                  ("SPC" . #x020)  ; Space
+                                  ("<escape>" . #xFF1B)
+                                  ("<return>" . #xFF0D)
+                                  ,@(mapcar (lambda (x) `(,(char-to-string x) . ,x))
+                                            (number-sequence ?0 ?9))))
+
+;; Select = 0xFF60
+;; #define XK_Select 0xff60  /* Select, mark */
+(defvar imbot-backend-composition-keys '(("C-d" . #xFFFF)
+                                       ("<deletechar>" . #xFFFF)
+                                       ("C-k" . (#xFFFF 1)) ; Shift+Delete
+                                       ("DEL" . #xFF08) ; BackSpace
+                                       ("<backspace>" . #xFF08)
+                                       ("<delete>" . #xFF08)
+                                       ("C-b" . #xFF51)   ; Left
+                                       ("C-f" . #xFF53)   ; Right
+                                       ("C-a" . #xFF50)   ; Home
+                                       ("C-e" . #xFF57))) ; End
+
+(defun imbot-backend-process-key (keysym &optional mask)
+  (fcitx-process-key keysym mask))
+
+;; (a(si) preedit, i cursorpos, a(si) auxUp, a(si) auxDown, a(ss) candidates,
+;; i candidateIndex, i layoutHint, b hasPrev, b hasNext)
+;; preedit	String	The current composition string (e.g., "nihao").
+;; cursorpos	Int32	Position of the cursor within the preedit string.
+;; auxUp	String	Auxiliary text above the input (often empty).
+;; auxDown	String	Auxiliary text below the input (often empty).
+;; candidates	List	A list of structs containing (String, Label).
+;; candidateIndex	Int32	The currently highlighted candidate index.
+;; layoutHint	Int32	UI layout suggestion (0 for horizontal, 1 for vertical).
+;; hasPrev	Boolean	Whether there is a previous page of candidates.
+;; hasNext	Boolean	Whether there is a next page of candidates.
+;; eg.
+;; ((("ni" 0)) 2 nil nil
+;;  (("1 " "你") ("2 " "拟") ("3 " "泥") ("4 " "霓") ("5 " "尼"))
+;;  0 0 nil t)
+(defun imbot-backend-update-tooltip ()
+  "Build candidate menu tooltip from imbot context."
+  (destructuring-bind (preedit cursorpos auxUp auxDown candidates candidateIndex layoutHint hasPrev hasNext) fcitx-ic-tooltip
+    (let (prompt-str page-str candidate-str tooltip)
+      (when preedit (setq prompt-str (with-temp-buffer
+                                       (insert (caar preedit))
+                                       (goto-char (1+ cursorpos))
+                                       (insert "˰")
+                                       (buffer-string)))
+            (when candidates
+              (setq page-str (mapconcat (lambda (c)
+                                          (if (car c) (cadr c) ""))
+                                        (list (list hasPrev "<") (list hasNext ">"))))
+              (setq candidate-str
+                    (mapconcat (lambda (c)
+                                 (let ((idx (string-trim (car c)))
+                                       (word (cadr c)))
+                                   (if (= (1- (string-to-number idx)) candidateIndex)
+                                       (format "[%s%s]" idx word)
+                                     (format "%s%s" idx word)))) candidates " ")))
+            (setq tooltip (concat prompt-str page-str "\n" candidate-str)))
+      (cons tooltip fcitx-ic-commit-string))))
+
+(defun imbot-backend-focusout ()
+  (fcitx-ic-call-method "FocusOut"))
+
+(defun imbot-backend-clear-composition ()
+  (fcitx-ic-call-method "Reset")
+  (setq fcitx-ic-commit-string nil))
+
+(defun imbot-backend-cleanup ()
+  (fcitx-ic-call-method "DestroyIC"))
+
+(defun imbot-backend-escape ()
+  "Clear the composition."
+  (interactive)
+  ;; send escape
+  (imbot--update 65307 0))
+
+(provide 'backend-fcitx-dbus)
---
+++ imbot.el
@@ -0,0 +1,237 @@
+;;; imbot.el *- lexical-binding: t; -*-
+
+(require 'seq)
+(require 'dash)
+(require 'posframe)
+
+(defgroup imbot nil
+  "imbot is a smart input method"
+  :group 'imbot)
+
+(defvar imbot-backend 'backend-fcitx-dbus
+  "definition for imbot-backend functions")
+
+(require imbot-backend)
+
+(defun imbot--predicate-program-mode-p ()
+  "当前为`prog-mode'或`conf-mode'，且光标在注释或字符串当中。"
+  (when (derived-mode-p 'prog-mode 'conf-mode)
+    (not (or (nth 3 (syntax-ppss))
+             (nth 4 (syntax-ppss))))))
+
+(defvar imbot--default-cursor '(bar . 4))
+(setq-default cursor-type imbot--default-cursor)
+(setq-default cursor-in-non-selected-windows 'hollow)
+
+(defvar imbot--overlay nil
+  "Inline english overlay.")
+
+(defun imbot--delete-overlay ()
+  (delete-overlay imbot--overlay)
+  (setq cursor-type imbot--default-cursor)
+  (setq imbot--overlay nil))
+
+(defvar imbot--inline-cursor '(hbar . 4)
+  "Inline english cursor.")
+
+(defface imbot--inline-face '((t (:weight bold :box nil :inverse-video nil)))
+  "Face to show inline english (input method temperarily disabled) is active.")
+
+(defun imbot--predicate-english-context-p ()
+  "Return t if English should be inputed at cursor point."
+  ;; (message "real this command %s" real-this-command)
+  (unless (or (eq real-this-command 'imbot--english-inline-deactivate)
+              (eq real-this-command 'imbot--english-inline-quit)
+              (eq real-this-command 'toggle-input-method))
+    (let* ((visual-line-beginning (line-beginning-position))
+           (point (point))
+           (overlay-active (overlayp imbot--overlay))
+           (english-context
+            (or
+             ;; 中文后面紧接1个空格切换到英文输入
+             ;; \cC represents any character of category “C”, according to “M-x describe-categories”
+             (looking-back "\\cC " (max visual-line-beginning (- point 2)))
+             (string-match "^\\s-*[0-9]+$" (buffer-substring-no-properties visual-line-beginning point))
+             ;; (looking-at-p "^\\*")    ; org heading
+             (looking-back "[a-zA-Z\\-]" (max visual-line-beginning (1- point))))))
+      (if overlay-active
+          (if english-context
+              (progn (move-overlay imbot--overlay visual-line-beginning (line-end-position))
+                     (message "Activate input method with [return]. Quit with [C-g]"))
+            (imbot--delete-overlay))
+        (when english-context
+          (setq imbot--overlay (make-overlay visual-line-beginning (line-end-position) nil t t))
+          (setq cursor-type imbot--inline-cursor)
+          (overlay-put imbot--overlay 'priority 900)
+          (overlay-put imbot--overlay
+                       'face 'imbot--inline-face)
+          (overlay-put imbot--overlay
+                       'keymap (let ((keymap (make-sparse-keymap)))
+                                 (define-key keymap (kbd "C-g")
+                                             #'imbot--english-inline-quit)
+                                 (define-key keymap (kbd "RET")
+                                             #'imbot--english-inline-deactivate)
+                                 (define-key keymap (kbd "<return>")
+                                             #'imbot--english-inline-deactivate)
+                                 (define-key keymap (kbd "C-\\")
+                                             #'imbot--english-inline-deactivate)
+                                 keymap))))
+      english-context)))
+
+(defun imbot--english-inline-deactivate ()
+  "Deactivate the inline english overlay."
+  (interactive)
+  (when (overlayp imbot--overlay)
+    (imbot--delete-overlay))
+  (setq imbot--suppressed nil))
+
+(defun imbot--english-inline-quit ()
+  "Quit the inline english overlay."
+  (interactive)
+  (when imbot--overlay
+    (imbot--delete-overlay)
+    (imbot--deactivate)))
+
+(defvar imbot--disable-predicates
+  '(imbot--predicate-english-context-p
+    imbot--predicate-program-mode-p))
+
+(defvar imbot--suppressed nil)
+(make-variable-buffer-local 'imbot--suppressed)
+
+(defun imbot--suppress-check ()
+  (if (equal input-method-function 'imbot-input-method)
+      (let ((suppressed (or (string-match " *temp*" (buffer-name))
+                            (seq-find 'funcall imbot--disable-predicates nil))))
+        (set-cursor-color "red")
+        (if suppressed
+            (setq cursor-type imbot--inline-cursor)
+          (setq cursor-type imbot--default-cursor))
+        (unless (equal imbot--suppressed suppressed)
+          (redisplay t)
+          (setq imbot--suppressed suppressed)))
+    (imbot--restore-cursor)))
+
+(defun imbot--text-read-only-p ()
+  "Return t if the text at point is read-only."
+  ;; NOTE: 在 widget 输入框存在的情况下，即使 buffer 是只读的，widget 输入
+  ;; 框也有可能要输入文本，EWW 就存在类似情况。
+  (and (get-pos-property (point) 'read-only)
+       (not (or inhibit-read-only
+                (get-pos-property (point) 'inhibit-read-only)))))
+
+(defface imbot--tooltip-face
+  '((((background light)) :background "#bfffff") (t :background "#400000"))
+  "Face with a (presumably) dimmed background for popup.")
+
+(defvar imbot--posframe-buffer " *imbot-posframe*"
+  "The buffer name for candidate posframe.")
+
+(defun imbot--tooltip-posframe (tooltip)
+  (if tooltip
+      (posframe-show imbot--posframe-buffer
+                   :foreground-color (face-attribute 'imbot--tooltip-face :foreground)
+                   :background-color (face-attribute 'imbot--tooltip-face :background)
+                   :string tooltip)
+    (posframe-hide imbot--posframe-buffer)))
+
+(defvar imbot--overriding nil)
+
+(defun imbot--map-set ()
+  (setq overriding-terminal-local-map imbot--map)
+  (setq imbot--overriding t))
+
+(defun imbot--map-unset ()
+  (setq overriding-terminal-local-map nil)
+  (setq imbot--overriding nil))
+
+(defun imbot--update (key state)
+  (let ((handled (imbot-backend-process-key key state)))
+    (with-silent-modifications
+      (unwind-protect
+          ;; commit is still nil when composition is active
+          (if handled
+              (let* ((output (imbot-backend-update-tooltip))
+                     (tooltip (car output))
+                     (commit (cdr output)))
+                (if tooltip
+                    (imbot--map-set)
+                  (imbot--map-unset))
+                (imbot--tooltip-posframe tooltip)
+                (when commit
+                  (insert commit)
+                  (setq fcitx-ic-commit-string nil)))
+            (list key))))))
+
+(defun imbot--activate (&optional _name)
+  (unless buffer-read-only
+    (setq-local input-method-function 'imbot-input-method)
+    (setq-local deactivate-current-input-method-function #'imbot--deactivate)
+    (add-hook 'post-command-hook 'imbot--suppress-check)
+    (advice-add 'keyboard-quit :after 'imbot--map-unset)
+    (imbot-backend-activate)
+    (add-hook 'kill-emacs-hook 'imbot-backend-cleanup)
+    (redisplay t)))
+
+;; Another special face is the cursor face.
+;; On graphical displays, the background color of this face is used to draw the text cursor.
+;; None of the other attributes of this face have any effect.
+;; As the foreground color for text under the cursor is taken from the background color of the underlying text.
+;; On text terminals, the appearance of the text cursor is determined by the terminal, not by the cursor face.
+(defun imbot--restore-cursor ()
+  (custom-set-faces
+   '(cursor ((t (:inherit font-lock-keyword-face)))))
+  (setq cursor-type imbot--default-cursor))
+
+(defun imbot--deactivate ()
+  (kill-local-variable 'input-method-function)
+  (remove-hook 'post-command-hook 'imbot--suppress-check)
+  (advice-remove 'keyboard-quit 'imbot--map-unset)
+  (imbot-backend-escape)
+  (imbot-backend-focusout)
+  (imbot--restore-cursor)
+  (redisplay t))
+
+(defun imbot--send-functional-key ()
+  (interactive)
+  (let* ((keyseq (vector last-input-event))
+         (keyseq-name (key-description keyseq))
+         (state 0)
+         (keysym (cdr (or (assoc keyseq-name imbot-backend-menu-keys)
+                          (assoc keyseq-name imbot-backend-composition-keys))))
+         key)
+    (if (listp keysym)
+        (setq state (cadr keysym)
+              key (car keysym))
+      (setq key keysym))
+    ;; (message "function key is %s" keysym)
+    (imbot--update key state)))
+
+(defvar imbot--map
+  (let ((map (make-sparse-keymap)))
+    (dolist (i (append imbot-backend-menu-keys imbot-backend-composition-keys))
+      (define-key map (kbd (car i)) 'imbot--send-functional-key))
+    (define-key map (kbd "C-g") 'imbot-backend-escape)
+    map))
+
+(defun imbot-input-method (key)
+  "Process character KEY with input method, other keys not handled."
+  (if (or imbot--suppressed
+          ;; (lookup-key overriding-terminal-local-map (vector key))
+          ;; (eq (cadr overriding-terminal-local-map) universal-argument-map)
+          ;; (and overriding-terminal-local-map
+          ;;      (not (equal (cadr overriding-terminal-local-map) imbot--map)))
+          (and (or overriding-local-map overriding-terminal-local-map)
+               (not imbot--overriding))
+          ;; upper case letter
+          ;; (and (> key 64) (< key 91))
+          ;; (not (alpha-char-p key))
+          (imbot--text-read-only-p))
+      (list key)
+    (imbot--update key 0)))
+
+(register-input-method "imbot" "euc-cn" 'imbot--activate "ㄓ" "smart input method")
+
+(provide 'imbot)
+
+;;; imbot.el ends here
